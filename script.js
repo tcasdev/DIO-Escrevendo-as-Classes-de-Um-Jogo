1dc8744a55947bf86a34d215ab5ec3f94e5b8801
Thais Aguiar <227265729+tcasdev@users.noreply.github.com>
2026-02-10 15:34:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+class HeroiDeterminado {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let tipoAtaque = "";
+        switch (this.tipo) {
+            case "mago":
+                tipoAtaque = "magia";
+                break;
+            case "guerreiro":
+                tipoAtaque = "espada";
+                break;
+            case "monge":
+                tipoAtaque = "artes marciais";
+                break;
+            case "ninja":
+                tipoAtaque = "shuriken";
+                break;
+            default:
+                tipoAtaque = "ataque desconhecido";
+        }
+        return tipoAtaque;
+    }
+}
+
+
+let heroiMagoDeFogo = new HeroiDeterminado("Ryuu", 25, "mago");
+
+let ataque = heroiMagoDeFogo.atacar();
+
+console.log(`O ${heroiMagoDeFogo.tipo} atacou usando ${ataque}`);
 
